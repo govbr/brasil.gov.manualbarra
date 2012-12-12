70c02ae2144ab6643e519e0c55e1d696c4433fd8
"thiago.silva" <thiago.silva@planejamento.gov.br>
2012-12-12 14:17:17 -0200

    commit inicial

---
+++ js/effects.js
@@ -0,0 +1,30 @@
+﻿// desenvolvido por Bernard De Luna - www.bernarddeluna.com  / @bernarddeluna
+$(document).ready(function() {
+	
+	// aumentando a fonte
+	$(".aumentar").click(function () {
+		var size = $("#principal").css('font-size');
+		
+		size = size.replace('px', '');
+		size = parseInt(size) + 1.4;
+		
+		$("#principal").animate({'font-size' : size + 'px'});
+	});
+	
+	//diminuindo a fonte
+	$(".diminuir").click(function () {
+		var size = $("#principal").css('font-size');
+		
+		size = size.replace('px', '');
+		size = parseInt(size) - 1.4;
+		
+		$("#principal").animate({'font-size' : size + 'px'});
+	});
+	
+	// resetando a fonte
+	$(".normal").click(function () {		
+		$("#principal").animate({'font-size' : '11px'});
+	});
+
+	
+});
